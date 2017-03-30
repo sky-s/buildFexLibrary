e75b8dbcda1f58c400f083878984edbc89ef6eed
Sky Sartorius <Sky@xwing.com>
2017-03-29 18:57:16 -0700

    added scl and goto and alphebetized

--- myFexList.m
+++ myFexList.m
@@ -8,41 +8,44 @@ function fileList = myFexList()
 
 
 % Default list for variety of general uses:
-fileList = {'carpetplot',52272
-    'CopyPaste',28016
-    'cprintf',24093
-    'disperse',33866
-    'explorestruct',7828
-    'export_fig',23629
-    'ezyfit',10176 % Note: latest version not on FEX
-    'git',29154
-    'polygeom',319
-    'progressbar',6922
-    'regexpBuilder',41899
-    'validatedinput',53553
-    'fevaln',53552
-    'bisection',28150
-    'blend',56530
-    'buildFEXlibrary',54832
-    'Link',48638
-    'units',38977
-    'dispdisp',48637
-    'tau',48636
-    'lyt',44040
-    'cch',43264
-    'speedtester',43250
-    'mcd',44043
-    'mkxlsfunc',40404
-    'randraw',7309
-    'randt',61261
-    'is___',61262
-    'cosspace',28337
-    'sinspace',28358
-    'linspace3',28553
-    'search_luckysearch',41357
-    'showOrigin',61632
-    'simps',25754
-    'styleguide',40795
-    'txtmenu',28285
-    'vgrid',56529
+fileList = {
+    'bisection'             28150
+    'blend'                 56530
+    'buildFEXlibrary'       54832
+    'carpetplot'            52272
+    'cch'                   43264
+    'CopyPaste'             28016
+    'cosspace'              28337
+    'cprintf'               24093
+    'dispdisp'              48637
+    'disperse'              33866
+    'explorestruct'          7828
+    'export_fig'            23629
+    'ezyfit'                10176
+    'fevaln'                53552
+    'git'                   29154
+    'goto'                   3145
+    'is___'                 61262
+    'Link'                  48638
+    'linspace3'             28553
+    'lyt'                   44040
+    'mcd'                   44043
+    'mkxlsfunc'             40404
+    'polygeom'                319
+    'progressbar'            6922
+    'randraw'                7309
+    'randt'                 61261
+    'regexpBuilder'         41899
+    'scl'                   56624
+    'search_luckysearch'    41357
+    'showOrigin'            61632
+    'simps'                 25754
+    'sinspace'              28358
+    'speedtester'           43250
+    'styleguide'            40795
+    'tau'                   48636
+    'txtmenu'               28285
+    'units'                 38977
+    'validatedinput'        53553
+    'vgrid'                 56529
     };
